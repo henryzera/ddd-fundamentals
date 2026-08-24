d3f73236b3f932e9626b6cd8edd5fa0abd764ed1
Emanuel Henry <emanuelhenrysc@gmail.com>
2026-08-24 18:44:12 -0300

    feat: external dependencies

--- src/domain/use-cases/answer-question.ts
+++ src/domain/use-cases/answer-question.ts
@@ -1,4 +1,5 @@
 import { Answer } from "../entities/answer.js"
+import type { AnswersRepository } from "../repositories/answers-repository.js"
 
 interface AnswerQuestionUseCaseRequest {
     instructorId: string
@@ -7,8 +8,18 @@ interface AnswerQuestionUseCaseRequest {
 }
 
 export class AnswerQuestionUseCase {
-    execute({ instructorId, questionId, content}: AnswerQuestionUseCaseRequest) {
-        const answer = new Answer(content)
+    constructor(
+        private answerRepository: AnswersRepository
+    ){}
+    
+    async execute({ instructorId, questionId, content}: AnswerQuestionUseCaseRequest) {
+        const answer = new Answer({
+            content,
+            authorId: instructorId,
+            questionId
+        })
+
+        await this.answerRepository.create(answer)
 
         return answer
     }
