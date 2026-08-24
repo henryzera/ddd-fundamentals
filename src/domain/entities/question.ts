37cb4257be94b6628fc134e4edcc7745e650f32a
Emanuel Henry <emanuelhenrysc@gmail.com>
2026-08-24 19:45:30 -0300

    feat: entity class

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,22 +1,13 @@
-import { randomUUID } from "node:crypto"
+import type { Slug } from "./value-objects/slug.js"
+import { Entity } from "../../core/entities/entity.js"
 
 interface QuestionProps {
     title: string
+    slug: Slug
     content: string
     authorId: string
 }
 
-export class Question {
-    public title: string
-    public content: string
-    public id: string
-    public authorId: string
+export class Question extends Entity<QuestionProps> {
 
-    constructor(props: QuestionProps, id?: string){
-        this.title = props.title
-        this.content = props.content
-        this.authorId = props.authorId
-        this.id = id ?? randomUUID()
-        
-    }
 }
